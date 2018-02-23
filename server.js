36aa81920e1586fd8de85424ea8de4fed6ebb81b
Benjamin Roy <benji015@hotmail.com>
2018-02-23 14:39:19 -0500

    Access Nodejs server on Lan (#6)

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const config = require('./webpack.config');
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', (err) => {
+  historyApiFallback: true,
+  compress: true,
+  disableHostCheck: true
+}).listen(3000, '0.0.0.0', (err) => {
   if (err) {
     console.log(err);
   }
