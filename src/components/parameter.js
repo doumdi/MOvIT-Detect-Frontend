fc811c6b67247318ac391fd82d0a8e1913c539de
Austin-Didier Tran <tran.austin.didier@gmail.com>
2018-03-13 11:51:30 -0400

    Feature/movit 168 (#17)
    * Modifications demandées par Charles Document 4
    
    * modifications du document 4 demandées par Charles

--- src/components/parameter.js
+++ src/components/parameter.js
@@ -31,42 +31,98 @@ class Parameters extends Component {
             <Checkbox inputId="dataAgreement" onChange={(e) => this.setState({ dataAgreement: e.checked })} checked={this.state.dataAgreement} />
             <label htmlFor="dataAgreement">{T.translate(`parameters.dataAgreement.${this.props.language}`)}</label>
           </div>
+          {this.state.dataAgreement ? '' :
+            <div>
+              <div className="col-sm-12" >
+                <h5>{T.translate(`parameters.doNotSend.${this.props.language}`)}:</h5>
+              </div>
+              <div className="col-sm-12" >
+                <RadioButton
+                  inputId="doNotSendDay" value="day" onChange={(e) => this.setState({ doNotDisturb: e.value })}
+                  checked={this.state.doNotDisturb === 'day'}
+                />
+                <label htmlFor="doNotSendDay">24 {T.translate(`time.hours.${this.props.language}`)}</label>
+              </div>
+              <div className="col-sm-12" >
+                <RadioButton
+                  inputId="doNotSendWeek" value="week" onChange={(e) => this.setState({ doNotDisturb: e.value })}
+                  checked={this.state.doNotDisturb === 'week'}
+                />
+                <label htmlFor="doNotSendWeek">1 {T.translate(`time.week.${this.props.language}`)}</label>
+              </div>
+              <div className="col-sm-12" >
+                <RadioButton
+                  inputId="doNotSendMonth" value="month" onChange={(e) => this.setState({ doNotDisturb: e.value })}
+                  checked={this.state.doNotDisturb === 'month'}
+                />
+                <label htmlFor="doNotSendMonth">1 {T.translate(`time.month.${this.props.language}`)}</label>
+              </div>
+            </div>
+          }
           <div className="col-sm-12" >
             <Checkbox inputId="lightAgreement" onChange={(e) => this.setState({ lightAgreement: e.checked })} checked={this.state.lightAgreement} />
             <label htmlFor="lightAgreement">{T.translate(`parameters.lightAgreement.${this.props.language}`)}</label>
           </div>
+          {this.state.lightAgreement ? '' :
+            <div>
+              <div className="col-sm-12" >
+                <h5>{T.translate(`parameters.doNotLightUp.${this.props.language}`)}:</h5>
+              </div>
+              <div className="col-sm-12" >
+                <RadioButton
+                  inputId="doNotLightUpDay" value="day" onChange={(e) => this.setState({ doNotDisturb: e.value })}
+                  checked={this.state.doNotDisturb === 'day'}
+                />
+                <label htmlFor="doNotLightUpDay">24 {T.translate(`time.hours.${this.props.language}`)}</label>
+              </div>
+              <div className="col-sm-12" >
+                <RadioButton
+                  inputId="doNotLightUpWeek" value="week" onChange={(e) => this.setState({ doNotDisturb: e.value })}
+                  checked={this.state.doNotDisturb === 'week'}
+                />
+                <label htmlFor="doNotLightUpWeek">1 {T.translate(`time.week.${this.props.language}`)}</label>
+              </div>
+              <div className="col-sm-12" >
+                <RadioButton
+                  inputId="doNotLightUpMonth" value="month" onChange={(e) => this.setState({ doNotDisturb: e.value })}
+                  checked={this.state.doNotDisturb === 'month'}
+                />
+                <label htmlFor="doNotLightUpMonth">1 {T.translate(`time.month.${this.props.language}`)}</label>
+              </div>
+            </div>
+          }
           <div className="col-sm-12" >
             <Checkbox inputId="notificationAgreement" onChange={(e) => this.setState({ notificationAgreement: e.checked })} checked={this.state.notificationAgreement} />
             <label htmlFor="notificationAgreement">{T.translate(`parameters.notificationAgreement.${this.props.language}`)}</label>
           </div>
-          <div className="col-sm-12" >
-            <hr />
-          </div>
-          <div className="col-sm-12" >
-            <h4>{T.translate(`parameters.doNotDisturb.${this.props.language}`)}:</h4>
-          </div>
-          <div className="col-sm-12" >
-            <RadioButton
-              inputId="doNotDisturbDay" value="day" onChange={(e) => this.setState({ doNotDisturb: e.value })}
-              checked={this.state.doNotDisturb === 'day'}
-            />
-            <label htmlFor="doNotDisturbDay">24 {T.translate(`time.hours.${this.props.language}`)}</label>
-          </div>
-          <div className="col-sm-12" >
-            <RadioButton
-              inputId="doNotDisturbWeek" value="week" onChange={(e) => this.setState({ doNotDisturb: e.value })}
-              checked={this.state.doNotDisturb === 'week'}
-            />
-            <label htmlFor="doNotDisturbWeek">1 {T.translate(`time.week.${this.props.language}`)}</label>
-          </div>
-          <div className="col-sm-12" >
-            <RadioButton
-              inputId="doNotDisturbMonth" value="month" onChange={(e) => this.setState({ doNotDisturb: e.value })}
-              checked={this.state.doNotDisturb === 'month'}
-            />
-            <label htmlFor="doNotDisturbMonth">1 {T.translate(`time.month.${this.props.language}`)}</label>
-          </div>
-
+          {this.state.notificationAgreement ? '' :
+            <div>
+              <div className="col-sm-12" >
+                <h5>{T.translate(`parameters.doNotReceive.${this.props.language}`)}:</h5>
+              </div>
+              <div className="col-sm-12" >
+                <RadioButton
+                  inputId="doNotReceiveDay" value="day" onChange={(e) => this.setState({ doNotDisturb: e.value })}
+                  checked={this.state.doNotDisturb === 'day'}
+                />
+                <label htmlFor="doNotReceiveDay">24 {T.translate(`time.hours.${this.props.language}`)}</label>
+              </div>
+              <div className="col-sm-12" >
+                <RadioButton
+                  inputId="doNotReceiveWeek" value="week" onChange={(e) => this.setState({ doNotDisturb: e.value })}
+                  checked={this.state.doNotDisturb === 'week'}
+                />
+                <label htmlFor="doNotReceiveWeek">1 {T.translate(`time.week.${this.props.language}`)}</label>
+              </div>
+              <div className="col-sm-12" >
+                <RadioButton
+                  inputId="doNotReceiveMonth" value="month" onChange={(e) => this.setState({ doNotDisturb: e.value })}
+                  checked={this.state.doNotDisturb === 'month'}
+                />
+                <label htmlFor="doNotReceiveMonth">1 {T.translate(`time.month.${this.props.language}`)}</label>
+              </div>
+            </div>
+          }
         </div>
         <div className="col-sm-9 text-right">
           <button type="submit" className="btn btn-lg">{T.translate(`cancel.${this.props.language}`)}</button>
