599b56a7e958623bfdc9c5c0c64399e73980ab53
Austin-Didier Tran <tran.austin.didier@gmail.com>
2018-04-14 15:42:44 -0400

    Send goals to server (#46)

--- webpack.config.js
+++ webpack.config.js
@@ -64,6 +64,11 @@ module.exports = {
   devtool: 'eval-source-map',
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
     new webpack.NoErrorsPlugin(),
   ],
   module: {
