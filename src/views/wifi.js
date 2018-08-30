6979c1ef96e753e0184b7a83176067617b740c3a
Unknown <gabboucher93@gmail.com>
2018-08-30 11:42:51 -0400

    Add wifi page

---
+++ src/views/wifi.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { T } from '../utilities/translator';
+import SubmitButtons from '../components/submitButtons';
+import LogoText from '../components/logoText';
+import { URL } from '../redux/applicationReducer';
+
+class Wifi extends Component {
+
+  static propTypes = {
+    language: PropTypes.string.isRequired,
+    history: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      wifi: '',
+      password: '',
+    };
+    this.changeWifi = this.changeWifi.bind(this);
+    this.changePassword = this.changePassword.bind(this);
+  }
+
+  changeWifi(wifiName) {
+    this.setState({ ...this.state, wifi: wifiName });
+  }
+
+  changePassword(passwordString) {
+    this.setState({ ...this.state, password: passwordString });
+  }
+
+  save() {
+    axios.post(`${URL}wifi`, this.state)
+      .then(() => { console.log('success'); this.props.history.push('/goals'); })
+      .catch(error => console.log(error));
+  }
+
+  cancel() {
+    this.setState({ wifi: '', password: '' });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="col-md-12">
+          <div className="form-horizontal">
+            <legend className="text-center header"><h2>{T.translate(`wifi.${this.props.language}`)}</h2></legend>
+            <LogoText
+              iconClass="fa fa-wifi"
+              placeHolder={T.translate(`wifi.name.${this.props.language}`)}
+              value={this.state.wifi}
+              onChange={this.changeWifi}
+            />
+            <LogoText
+              iconClass="fa fa-key"
+              placeHolder={T.translate(`login.password.${this.props.language}`)}
+              value={this.state.password}
+              onChange={this.changePassword}
+            />
+          </div>
+          <SubmitButtons
+            onSave={this.save.bind(this)}
+            onCancel={this.cancel.bind(this)}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    language: state.applicationReducer.language,
+  };
+}
+
+export default connect(mapStateToProps)(Wifi);
