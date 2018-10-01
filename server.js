ad337c9199e056cff52b5b0292514f9d5c667733
Austin-Didier Tran <tran.austin.didier@gmail.com>
2018-09-30 20:17:20 -0400

    Months now take first date

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config');
 
 const port = process.env.PORT || 3000;
-const host = process.env.host || 'movit-plus.herokuapp.com';
+const host = process.env.host || process.env.NODE_ENV === 'demo' ? 'movit-plus.herokuapp.com' : '0.0.0.0';
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
