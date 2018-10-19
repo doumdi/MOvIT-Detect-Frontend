9b316fe1526eeb518d4532e0172f3f80c498d6ce
Benjamin Roy <benji015@hotmail.com>
2018-10-19 14:50:19 -0400

    Added textRecommandation tests

--- tests/components/textRecommandationTests.js
+++ tests/components/textRecommandationTests.js
@@ -1,14 +1,48 @@
+/**
+ * @author Gabriel Boucher
+ * @author Anne-Marie Desloges
+ * @author Austin-Didier Tran
+ * @author Benjamin Roy
+ */
+
 import React from 'react';
-import TestUtils from 'react-dom/test-utils';
 import PropTypes from 'prop-types';
-import TextRecommandation from '../../src/components/textRecommendation';
+import { Checkbox } from 'primereact/components/checkbox/Checkbox';
+import { InputText } from 'primereact/components/inputtext/InputText';
+import configureMockStore from 'redux-mock-store';
+import sinon from 'sinon';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+
+import TextRecommendation from '../../src/components/textRecommendation';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('TextRecommandation Tests', () => {
-  it('should have proptypes', () => {
-    // Actual value
-    const actualValue = TextRecommandation.WrappedComponent.propTypes;
+  const onChangeActiveSpy = sinon.spy();
+  const onChangeValueSpy = sinon.spy();
+  const initialState = { applicationReducer: { language: 'en' } };
+  const mockStore = configureMockStore();
+  const store = mockStore(initialState);
+  const props = {
+    recActive: true,
+    title: 'Test',
+    maxAngle: 45,
+    value: 0,
+    language: 'en',
+    onChangeActive: (value) => { onChangeActiveSpy(value); },
+    onChangeValue: (value) => { onChangeValueSpy(value); },
+  };
 
-    // Expected value
+  beforeEach(() => {
+    onChangeValueSpy.resetHistory();
+    onChangeActiveSpy.resetHistory();
+  });
+
+  it('should have proptypes', () => {
+    const actualValue = TextRecommendation.WrappedComponent.propTypes;
+
     const expectedValue = {
       language: PropTypes.string.isRequired,
       recActive: PropTypes.bool.isRequired,
@@ -18,7 +52,61 @@ describe('TextRecommandation Tests', () => {
       onChangeValue: PropTypes.func.isRequired,
     };
 
-    // Test
     expect(JSON.stringify(actualValue)).toEqual(JSON.stringify(expectedValue));
+  });
+
+  it('should trigger onChangeActive when simulating a change event on checkbox', () => {
+    const wrapper = shallow(<TextRecommendation store={store} {...props} />).dive();
+
+    wrapper.find(Checkbox).simulate('change', { checked: false });
+
+    expect(onChangeActiveSpy.calledOnce).toEqual(true);
+    expect(onChangeActiveSpy.getCalls()[0].args[0]).toEqual(false);
+  });
+
+  it('should trigger onChangeValue with an empty string when simulating a change event on checkbox', () => {
+    const wrapper = shallow(<TextRecommendation store={store} {...props} />).dive();
+
+    wrapper.find(Checkbox).simulate('change', { checked: false });
+
+    expect(onChangeValueSpy.calledOnce).toEqual(true);
+    expect(onChangeValueSpy.getCalls()[0].args[0]).toEqual('');
+  });
+
+  it('should trigger onChangeValue with a parameter when simulating a change event on checkbox', () => {
+    const wrapper = shallow(<TextRecommendation store={store} {...props} />).dive();
+
+    wrapper.find(Checkbox).simulate('change', { checked: true });
+
+    expect(onChangeValueSpy.calledOnce).toEqual(true);
+    expect(onChangeValueSpy.getCalls()[0].args[0]).toEqual('recommendations.tiltAsNeeded.en');
+  });
+
+  it('should trigger onChangeValue when simulating a change event on InputText', () => {
+    const wrapper = shallow(<TextRecommendation store={store} {...props} />).dive();
+
+    wrapper.find(InputText).simulate('change', { target: { value: 'Test' } });
+
+    expect(onChangeValueSpy.calledOnce).toEqual(true);
+    expect(onChangeValueSpy.getCalls()[0].args[0]).toEqual('Test');
+  });
+
+  it('should match the snapshot when the recommandation is active', () => {
+    const wrapper = shallow(<TextRecommendation store={store} {...props} />).dive();
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('should match the snapshot when the recommandation is inactive', () => {
+    const inactiveProps = {
+      recActive: false,
+      title: 'Test',
+      maxAngle: 45,
+      value: 10,
+      language: 'en',
+    };
+    const wrapper = shallow(<TextRecommendation store={store} {...inactiveProps} />).dive();
+
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
