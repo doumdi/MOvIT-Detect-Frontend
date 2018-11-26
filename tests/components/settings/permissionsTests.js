f78c2679f28591dbf9fe2a024fb9b2b921e21fab
GBoucher42 <gabboucher93@gmail.com>
2018-11-26 12:08:53 -0500

    test permission and pressureRecPanel (#166)

--- tests/components/settings/permissionsTests.js
+++ tests/components/settings/permissionsTests.js
@@ -5,10 +5,24 @@
  * @author Benjamin Roy
  */
 
+import React from 'react';
 import PropTypes from 'prop-types';
+import configureMockStore from 'redux-mock-store';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+
 import Permissions from '../../../src/components/settings/permissions';
 
+Enzyme.configure({ adapter: new Adapter() });
+
 describe('Permissions Tests', () => {
+  const initialState = {
+    applicationReducer: { language: 'en' },
+  };
+  const mockStore = configureMockStore();
+  const store = mockStore(initialState);
+
   it('should have proptypes', () => {
     const actualValue = Permissions.WrappedComponent.propTypes;
 
@@ -20,5 +34,21 @@ describe('Permissions Tests', () => {
     };
 
     expect(JSON.stringify(actualValue)).toEqual(JSON.stringify(expectedValue));
+  });
+
+  it('should match the snapshot with no error', () => {
+    const props = {
+      hasErrors: false,
+    };
+    const wrapper = shallow(<Permissions store={store} {...props} />).dive();
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('should match the snapshot with errors', () => {
+    const props = {
+      hasErrors: true,
+    };
+    const wrapper = shallow(<Permissions store={store} {...props} />).dive();
+    expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
