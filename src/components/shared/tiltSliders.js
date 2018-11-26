b9f13b2feb38cbd86c10a2bf4ad414e8594932f6
Austin-Didier Tran <tran.austin.didier@gmail.com>
2018-11-26 12:50:53 -0500

    Fixed recommendation loading

--- src/components/shared/tiltSliders.js
+++ src/components/shared/tiltSliders.js
@@ -10,11 +10,12 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { T } from '../../utilities/translator';
 import SliderValue from './sliderValue';
+import { SEC_IN_MIN } from '../../utilities/constants';
 
 class TiltSliders extends Component {
   static propTypes = {
     language: PropTypes.string.isRequired,
-    tiltFrequecy: PropTypes.number,
+    tiltFrequency: PropTypes.number,
     tiltLength: PropTypes.number,
     tiltAngle: PropTypes.number,
     maxAngle: PropTypes.number,
@@ -28,7 +29,7 @@ class TiltSliders extends Component {
       <div className="col-12 col-md-10 ml-3">
         <SliderValue
           id="frequency-slider" min={0} max={180} onChange={this.props.onFrequencyChange}
-          value={this.props.tiltFrequecy} unit="min" title={T.translate(`recommendations.frequency.${this.props.language}`)}
+          value={this.props.tiltFrequency} unit="min" title={T.translate(`recommendations.frequency.${this.props.language}`)}
         />
         <SliderValue
           id="duration-slider" min={0} max={30} onChange={this.props.onLengthChange}
